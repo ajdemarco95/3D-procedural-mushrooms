c379d45536468c6719d62d0a37c0779185e266ec
AJ DeMarco <79189312+ajdemarco95@users.noreply.github.com>
2024-01-06 20:51:39 -0500

    sorta rendering a line

--- src/Mushroom.jsx
+++ src/Mushroom.jsx
@@ -1,51 +1,36 @@
 import { CatmullRomLine } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import * as THREE from 'three'
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 
 
 function Mushroom() {
 
     const geoRef = useRef();
     const meshRef = useRef();
-    const posRef = useRef();
 
-// Define basic settings
-  const vertRes = 10;
-  const angRes = 10;
-  const stemVert = [];
-  const stemIndices = [];
+  const curve = new THREE.CatmullRomCurve3( [
+	new THREE.Vector3( -10, 0, 10 ),
+	new THREE.Vector3( -5, 5, 5 ),
+	new THREE.Vector3( 0, 0, 0 ),
+	new THREE.Vector3( 5, -5, 5 ),
+	new THREE.Vector3( 10, 0, 10 )
+] );
 
-  const positions = new Float32Array([
-    1, 24, 0,
-    0, 1, 0,
-    -1, 0, 0,
-    3, -1, 0
-])
+const points = curve.getPoints( 100 );
 
-const normals = new Float32Array([
-    0, 0, 1,
-    0, 0, 1,
-    0, 0, 1,
-    0, 0, 1,
-])
 
-const colors = new Float32Array([
-    0, 1, 1, 1,
-    1, 0, 1, 1,
-    1, 1, 0, 1,
-    1, 1, 1, 1,
-])
+useEffect(() => {
+    geoRef.current.setFromPoints(points)
+}, [])
 
-const indices = new Uint16Array([
-    0, 1, 3,
-    2, 3, 1,
-])
+
+
 
 useFrame((state, delta) => {
-
-    posRef.current.needsUpdate = true;
-    
+    // posRef.current.needsUpdate = true;
+    // geoRef.current.setFromPoints(points)
+    console.log(curve.points)
 })
 
 
@@ -54,36 +39,35 @@ useFrame((state, delta) => {
     <>
     <mesh ref={meshRef}>
         <bufferGeometry ref={geoRef} >
-            <bufferAttribute
+            {/* <bufferAttribute
                 ref={posRef}
                 attach='attributes-position'
                 array={positions}
                 count={positions.length / 3}
                 itemSize={3}
-            />
-            <bufferAttribute
+            /> */}
+            {/* <bufferAttribute
                 attach='attributes-color'
                 array={colors}
                 count={colors.length / 3}
                 itemSize={3}
-            />
-            <bufferAttribute
+            /> */}
+            {/* <bufferAttribute
                 attach='attributes-normal'
                 array={normals}
                 count={normals.length / 3}
                 itemSize={3}
-            />
-            <bufferAttribute
+            /> */}
+            {/* <bufferAttribute
                 attach="index"
                 array={indices}
                 count={indices.length}
                 itemSize={1}
-            />
+            /> */}
         </bufferGeometry>
         <meshStandardMaterial
             vertexColors
             side={THREE.DoubleSide}
-            // wireframe={true}
         />
     </mesh>
 
