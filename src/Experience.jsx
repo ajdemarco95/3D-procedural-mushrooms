7fcd429ea37b540ea0ba85020f6d1c7445d07e6f
AJ DeMarco <79189312+ajdemarco95@users.noreply.github.com>
2024-01-17 18:13:46 -0500

    FIxed page title

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -10,7 +10,7 @@ function Experience() {
   return (
     <div className="w-full h-full fixed top-0 left-0  bg-slate-500">
       <Canvas>
-        <Perf />
+        {/* <Perf /> */}
 
         <OrbitControls />
 
